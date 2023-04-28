5244ea32e0c206d7c6464d655d5e89237fe6a4df
ciceroff <cicero.barrozo13@gmail.com>
2023-04-28 16:48:30 -0300

    ♻️ start adding new transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,24 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <HeaderContent>
-        DT Money
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
-      </HeaderContent>
+      <HeaderContent>DT Money</HeaderContent>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionButton>Nova Transação</NewTransactionButton>
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <Dialog.Overlay />
+          <Dialog.Content>
+            <Dialog.Title>Nova Transação</Dialog.Title>
+
+            <Dialog.Close></Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </HeaderContainer>
   )
 }
