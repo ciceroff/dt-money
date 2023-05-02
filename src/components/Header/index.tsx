7af6a71adf5d103c91fa8fb6f4d455bc0c59bbe4
ciceroff <cicero.barrozo13@gmail.com>
2023-05-02 12:04:12 -0300

    ✨ add project modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,20 @@
+import { NewTransactionModal } from '../NewTransactionModal'
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import * as Dialog from '@radix-ui/react-dialog'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <HeaderContent>DT Money</HeaderContent>
-      <Dialog.Root>
-        <Dialog.Trigger asChild>
-          <NewTransactionButton>Nova Transação</NewTransactionButton>
-        </Dialog.Trigger>
+      <HeaderContent>
+        DT Money
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
 
-        <Dialog.Portal>
-          <Dialog.Overlay />
-          <Dialog.Content>
-            <Dialog.Title>Nova Transação</Dialog.Title>
-
-            <Dialog.Close></Dialog.Close>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
+          <NewTransactionModal></NewTransactionModal>
+        </Dialog.Root>
+      </HeaderContent>
     </HeaderContainer>
   )
 }
